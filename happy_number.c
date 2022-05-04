befb7a6a57a92d73640b0a1c423f3a4e2046315c
21MH1A0599 <97426528+21MH1A0599@users.noreply.github.com>
2022-05-04 17:30:12 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int i,m=n,t=n,r,sum;     for(i=0;i<9;i++)     {         sum=0;         while(m>0)         {             r=m%10;             sum=sum+r*r;             m=m/10;         }         m=sum;     }     if(sum==1)     {         printf("True");     }     else     {         printf("False");     } }

--- happy_number.c
+++ happy_number.c
@@ -7,15 +7,15 @@ int main()
     for(i=0;i<9;i++)
     {
         sum=0;
-    while(m>0)
-    {
-        r=m%10;
-        sum=sum+r*r;
-        m=m/10;
+        while(m>0)
+        {
+            r=m%10;
+            sum=sum+r*r;
+            m=m/10;
+        }
+        m=sum;
     }
-    m=sum;
-}
-   if(sum==1)
+    if(sum==1)
     {
         printf("True");
     }
@@ -23,5 +23,4 @@ int main()
     {
         printf("False");
     }
-    
 }
