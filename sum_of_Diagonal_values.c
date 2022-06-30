27a23e50cf8402b83b181c71f21687f66f2810dd
21MH1A0599 <97426528+21MH1A0599@users.noreply.github.com>
2022-06-30 14:33:52 +0530

    #include<stdio.h> int main() {     int r,c,i,j,sum=0;     scanf("%d %d",&r,&c);     int a[r][c];     for(i=0;i<c;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&a[i][j]);         }     }     for(i=0;i<c;i++)     {         for(j=0;j<c;j++)         {             if(i==j || i+j==r-1)             {                 sum+=a[i][j];             }         }     }     printf("%d",sum); }

--- sum_of_Diagonal_values.c
+++ sum_of_Diagonal_values.c
@@ -3,21 +3,21 @@ int main()
 {
     int r,c,i,j,sum=0;
     scanf("%d %d",&r,&c);
-    int arr[r][c];
-     for(i=0;i<r;i++)
+    int a[r][c];
+    for(i=0;i<c;i++)
     {
         for(j=0;j<c;j++)
         {
-          scanf("%d",&arr[i][j]);
+            scanf("%d",&a[i][j]);
         }
     }
-     for(i=0;i<r;i++)
+    for(i=0;i<c;i++)
     {
         for(j=0;j<c;j++)
         {
             if(i==j || i+j==r-1)
             {
-                sum+=arr[i][j];
+                sum+=a[i][j];
             }
         }
     }
