8608341b5446f415f9ae7d5548d9de4ac1d24097
21MH1A0599 <97426528+21MH1A0599@users.noreply.github.com>
2022-07-11 19:13:21 +0530

    #include<stdio.h> int main() {     int i,j,r,c,sum;     scanf("%d %d",&r,&c);     int arr[r][c];     for(i=0;i<r;i++)     {         sum=0;         for(j=0;j<c;j++)         {             scanf("%d",&arr[i][j]);             sum+=arr[i][j];         }         printf("%d ",sum);     } }

--- Row_wise_sum_of_a_matrix.c
+++ Row_wise_sum_of_a_matrix.c
@@ -7,11 +7,11 @@ int main()
     for(i=0;i<r;i++)
     {
         sum=0;
-        for (j=0;j<c;j++)
+        for(j=0;j<c;j++)
         {
             scanf("%d",&arr[i][j]);
             sum+=arr[i][j];
         }
-    printf("%d ",sum);
+        printf("%d ",sum);
     }
 }
