eb95960c1214ee83b12abb9efd298c0f0460c2e1
21MH1A0599 <97426528+21MH1A0599@users.noreply.github.com>
2022-12-18 15:47:38 +0530

    #include<stdio.h> int main() {     int s=0;     char str[100];     scanf("%s",str);     for(int i=0;str[i]!=NULL;i++)     {         if(str[i]>='0' and str[i]<='9')         {             s+=str[i]-'0';         }     }     printf("%d",s); }

---
+++ Program_to_find_the_sum_of_all_numbers_present_in_the_string.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int s=0;
+    char str[100];
+    scanf("%s",str);
+    for(int i=0;str[i]!=NULL;i++)
+    {
+        if(str[i]>='0' and str[i]<='9')
+        {
+            s+=str[i]-'0';
+        }
+    }
+    printf("%d",s);
+}
