76641009970c12b69818f5168d5fb2321dfb0d70
21MH1A0599 <97426528+21MH1A0599@users.noreply.github.com>
2022-12-18 13:51:26 +0530

    #include<stdio.h> int main() {     int n,c;     scanf("%d",&n);     int arr[n];     for(int i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(int i=0;i<n;i++)     {         c=0;         for(int j=0;j<n;j++)         {             if(arr[i]==arr[j] and i!=j)             {                 c+=1;             }         }         if(c==0)         {             printf("%d",arr[i]);             break;         }     } }

---
+++ Single_Number.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n,c;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        c=0;
+        for(int j=0;j<n;j++)
+        {
+            if(arr[i]==arr[j] and i!=j)
+            {
+                c+=1;
+            }
+        }
+        if(c==0)
+        {
+            printf("%d",arr[i]);
+            break;
+        }
+    }
+}
